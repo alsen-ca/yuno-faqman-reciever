4730be7c2dbed241ae121d1bc11d47bc69d5ac4b
hamination <173510244+alsen-ca@users.noreply.github.com>
2026-01-20 20:28:04 +0100

    Fix db tag indexes not accepting empty values

--- internal/db/indexes.go
+++ internal/db/indexes.go
@@ -26,15 +26,15 @@ func EnsureTagIndexes(ctx context.Context, client *mongo.Client) error {
     index := []mongo.IndexModel {
         {
             Keys:    bson.M{"en_og": 1},
-            Options: options.Index().SetUnique(true).SetName("unique_en_tag"),
+            Options: options.Index().SetUnique(true).SetName("unique_en_tag").SetPartialFilterExpression(bson.M{"en_og": bson.M{"$exists": true, "$gt": ""}}),
         },
         {
             Keys:    bson.M{"de_trans": 1},
-            Options: options.Index().SetUnique(true).SetName("unique_de_tag"),
+            Options: options.Index().SetUnique(true).SetName("unique_de_tag").SetPartialFilterExpression(bson.M{"de_trans": bson.M{"$exists": true, "$gt": ""}}),
         },
         {
             Keys:    bson.M{"es_trans": 1},
-            Options: options.Index().SetUnique(true).SetName("unique_es_tag"),
+            Options: options.Index().SetUnique(true).SetName("unique_es_tag").SetPartialFilterExpression(bson.M{"es_trans": bson.M{"$exists": true, "$gt": ""}}),
         },
     }
     _, err := TagCollection(client).Indexes().CreateMany(ctx, index)
--- run-dev.sh
+++ run-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo nerdctl --address /run/k3s/containerd/containerd.sock run --rm \
-    -it \
+sudo nerdctl --address /run/k3s/containerd/containerd.sock run -d \
     --name faqman-reciever \
     --network faqman \
     -p 127.0.0.1:8221:8221 \
@@ -10,4 +9,4 @@ sudo nerdctl --address /run/k3s/containerd/containerd.sock run --rm \
     -v go-mod-cache:/go/pkg/mod \
     -w /app \
     golang:1.25.5-trixie \
-    bash
+    go run .
